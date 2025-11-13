9da35337c3a98af3127463c3601654e9d05958c4
abod1232 <abgost4@outlook.com>
2025-11-13 18:32:43 +0300

    Update wecimaPlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/wecimaPlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/wecimaPlugin.kt
@@ -3,7 +3,6 @@ package com.wecima
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
-import com.cimaclub.CimaClub
 
 @CloudstreamPlugin
 class CimaClubPlugin: Plugin() {
